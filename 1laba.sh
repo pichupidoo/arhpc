b6824b31167825e07a5a4aadf062cb1c9f58578d
Andrew <gosharak12345@mail.ru>
2024-09-14 14:51:26 +0700

    ready to push

--- 1laba.sh
+++ 1laba.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 
-# Вывод информации об ОС и ядре
 echo "===== OS and Kernel Information ====="
-uname -a                                      #команда uname выводит информацию о системе. Ключ -a (от слова "all") указывает, что нужно вывести всю доступную информацию
-cat /etc/os-release                           #Файл /etc/os-release содержит информацию о версии операционной системы. Команда cat выводит содержимое этого файла
+hostnamectl | grep "Operating System\|Kernel"                                                              
 echo ""
 
-# Вывод информации о процессоре
+
 echo "===== CPU Information ====="
-lscpu
+lscpu | grep "Имя модели\|CPU"
+lscpu | grep "L1i cache\|L2 cache\|L3 cache"
 echo ""
 
-# Вывод информации об оперативной памяти
+
 echo "===== Memory Information ====="
-free -h                                                    #Опция -h Она выводит объём памяти в удобных для чтения единицах измерения: килобайтах (K), мегабайтах (M), гигабайтах (G),
+free -h | awk '/Память:/   {print "Всего:" $2}'
+free -h | awk '/Память:/   {print "Использовано:" $3}'
+free -h | awk '/Память:/   {print "Доступно:" $4}'                                                 
 echo ""
 
-# Вывод информации о сетевых интерфейсах и скорости соединения
+
 echo "===== Network Interface Information ====="
 ip -o addr show | awk '/inet / {print "Interface: " $2 " IP: " $4}'
-ip -o link show | awk '{print "Interface: " $2 " MAC: " $17}' #выводит информацию об интерфейсах в формате одной строки для каждого интерфейса.       /link\/ether/: фильтрует строки, содержащие MAC-адрес (по шаблону link/ether).
-DEFAULT_INTERFACE=$(ip route show to default | awk '{print $5}')
-ethtool $DEFAULT_INTERFACE | grep "Speed"
+ip -o link show | awk '{print "Interface: " $2 " MAC: " $17}' 
+iwconfig | grep "Bit Rate"
 echo ""
 
-# Вывод информации о системных разделах
+
 echo "===== Disk Partition Information ====="
 df -h
 echo ""
-
-echo "Script completed successfully."
